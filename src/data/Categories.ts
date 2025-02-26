d263a6f9f8e2102f4f3927cbdfc2446482afa35c
igorntwari <igorntwari28@gmail.com>
2025-02-26 22:06:23 +0200

    refactor: separate data from component

---
+++ src/data/Categories.ts
@@ -0,0 +1,8 @@
+export const categories = [
+  { id: 1, name: "Phones", icon: "📱" },
+  { id: 2, name: "Computers", icon: "💻" },
+  { id: 3, name: "SmartWatch", icon: "⌚" },
+  { id: 4, name: "Camera", icon: "📷" },
+  { id: 5, name: "HeadPhones", icon: "🎧" },
+  { id: 6, name: "Gaming", icon: "🎮" },
+];
