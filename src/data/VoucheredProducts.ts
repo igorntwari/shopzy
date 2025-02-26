63c68e4fdaffde3dd8b126357939219eb74af28d
igorntwari <igorntwari28@gmail.com>
2025-02-26 22:53:05 +0200

    refactor: update voucher product

---
+++ src/data/VoucheredProducts.ts
@@ -0,0 +1,26 @@
+export const VoucheredProducts = [
+  {
+    id: 1,
+    name: "iPhone 14 Series",
+    discount: "Up to 10% off Voucher",
+    imageSrc: "/public/images/VouchersImage/FirstProduct.png",
+    alt: "iPhone 14",
+    link: "#iphone-14",
+  },
+  {
+    id: 2,
+    name: "Gaming Joy Stick",
+    discount: "Up to 15% off Voucher",
+    imageSrc: "/public/images/VouchersImage/SecondProduct.png",
+    alt: "Gaming Joy Stick",
+    link: "#Gaming-Joy-Stick",
+  },
+  {
+    id: 3,
+    name: "Apple Watch",
+    discount: "Up to 20% off Voucher",
+    imageSrc: "/public/images/VouchersImage/ThirdProduct.png",
+    alt: "Apple Watch",
+    link: "#apple-watch",
+  },
+];
